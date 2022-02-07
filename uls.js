613b43a0f7fe176da0b46e1bf614bbb56aa201fc
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-02-08 00:07:54 +0530

    Create uls.js

---
+++ uls.js
@@ -0,0 +1,42 @@
+var slider=document.querySelector('.slider-wrapper');
+var images=document.querySelectorAll('.slide');
+var imgsrc=[];
+var thumbnails=document.querySelector('.thumbnails');
+function addHidetoslide(){
+  for(let i =0;i<images.length;i++){
+    images[i].classList.add("hide");
+  }
+}
+function imagetoimgsrc(){
+  for(let i =0;i<images.length;i++){
+    imgsrc[i]=images[i].getAttribute("src");
+      console.log(imgsrc[i]);
+  }
+  addHidetoslide();
+};
+
+function makingthumbnails(){
+  thumbnails.innerHTML='';
+   for(let i =0;i<imgsrc.length;i++){
+     thumbnails.innerHTML= thumbnails.innerHTML + `
+      <img class="thumbnail" src="${imgsrc[i]}">
+    `;
+   }
+  console.log(thumbnails);
+}
+imagetoimgsrc();
+makingthumbnails();
+images[0].classList.remove("hide");
+function thumbClicks(){
+  var thumbnail=document.querySelectorAll('.thumbnail');
+let thumb;
+console.log(thumbnail);
+  
+thumbnail.forEach((thumb,index)=>{
+  thumb.addEventListener('click',(thumb)=>{
+    addHidetoslide();
+     images[index].classList.remove("hide");
+  });
+})
+}
+thumbClicks();
